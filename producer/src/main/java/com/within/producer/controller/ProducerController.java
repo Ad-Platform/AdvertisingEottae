5d97a1d784554f2936a4daf491ee4a9460734288
richard <richard.nam@within.co.kr>
2018-06-07 20:11:14 +0900

    Deleted .gradle

--- producer/src/main/java/com/within/producer/controller/ProducerController.java
+++ producer/src/main/java/com/within/producer/controller/ProducerController.java
@@ -1,4 +1,13 @@
 package com.within.producer.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class ProducerController {
+
+    @PostMapping("/")
+    public String test() {
+        return "";
+    }
 }
