08cf849d3bdd39c722b277ee0400f20d6ce38db5
richard <richard.nam@within.co.kr>
2018-06-07 21:03:52 +0900

    Added source.

--- producer/src/main/java/com/within/producer/ProducerApplication.java
+++ producer/src/main/java/com/within/producer/ProducerApplication.java
@@ -1,11 +1,22 @@
 package com.within.producer;
 
+import com.within.producer.service.Sender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProducerApplication {
+public class ProducerApplication implements CommandLineRunner {
     public static void main(String[] args) {
-        SpringApplication.run(ProducerApplication.class);
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+
+    @Autowired
+    private Sender sender;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        sender.send("Spring Kafka Producer and Consumer Example");
     }
 }
